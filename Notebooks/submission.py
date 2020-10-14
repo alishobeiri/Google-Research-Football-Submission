3f83584fae43a4bef1d46d1934764ab312dfc365
Paul Fournier <paul.fournier4@mail.mcgill.ca>
2020-10-14 09:52:45 +0200

    920

--- Notebooks/submission.py
+++ Notebooks/submission.py
@@ -2,40 +2,76 @@ from kaggle_environments.envs.football.helpers import *
 import math
 import numpy as np
 from scipy.spatial.distance import cdist
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 
 @human_readable_agent
 def agent(obs):
 
-    def distance(player_one, player_two):
-        # Compute the distance between two players
-        return math.hypot(player_one[0] - player_two[0], player_one[1] - player_two[1])
+    def distance(coord_one, coord_two):
+        '''
+        Compute the distance between two players
+        Args:
+            coord_one: Coordinates of the first entity (player, ball, goal ...)
+            coord_two: Coordinates of the first entity
+        Returns: Distance
+        '''
+        return np.linalg.norm(np.array(coord_one) - np.array(coord_two))
 
     def angle(coord_one, coord_two):
-        # Compute the angle between two coordinates
-        return math.degrees(math.atan2(
+        '''
+        Compute the angle between two coordinates
+        Args:
+            coord_one: Coordinates of the first entity (player, ball, goal ...)
+            coord_two: Coordinates of the first entity
+        Returns: Angle in degrees
+        '''
+        return np.degrees(np.arctan2(
             coord_two[1] - coord_one[1], coord_two[0] - coord_one[0]))
 
     def find_closest_opposition_to_ball():
-        # Find the index of the opposing player closest to the ball
+        '''
+        Find the opposing player closest to the ball
+        Returns: Index of a opponent
+        '''
+        # Store distance to each opponent
         distances = obs['right_team'].copy()
         for i, player_position in enumerate(obs['right_team']):
+            # Compute the distance between the ball and the player
             distances[i] = distance(obs['ball'], player_position)
+        # Get the index for which the distance is minimum
         return np.argmin(distances)
 
-    def closest_player(controlled_player_position, team='left_team'):
-        # Find the index of the player closest to the player in posession
-        # Use team = 'right_team' to get the closest opponent
+    def closest_player(player_position, team='left_team'):
+        '''
+        Find the player closest to the player in posession
+        Args:
+            player_position: Coordinates of the player in posession
+            team: name of the team in which we look for a player. Use team = 'right_team' to get the closest opponent and use team = 'left_team' to get the closest teammate
+        Returns: Index of a player
+        '''
+        # Store distance to each player
         distances = obs[team].copy()
         for i, teammate_position in enumerate(obs[team]):
-            dist = distance(
-                controlled_player_position, teammate_position)
+            # Compute the distance between the ball and the player
+            dist = distance(player_position, teammate_position)
+            # Set infinite distance for the distance between the player controlled and himself
             distances[i] = np.Inf if dist == 0 else dist
+        # Get the index for which the distance is minimum
         return np.argmin(distances)
 
     def coordinate_to_direction(coord_one, coord_two):
-        # Given coordinate between a player and anything (player, goal ...), output cardinal direction
+        '''
+        Find cardinal direction to get from an entity to another
+        Args:
+            coord_one: Coordinates of the first entity (player, ball, goal ...)
+            coord_two: Coordinates of the first entity
+        Returns: Cardinal direction
+        '''
+        # Compute angle between entities
         deg = angle(coord_one, coord_two)
+        # Convert to a direction
         if -22.5 <= deg and deg <= 22.5:
             return Action.Right
         elif 22.5 < deg and deg <= 67.5:
@@ -54,26 +90,43 @@ def agent(obs):
             return Action.BottomRight
 
     def direction_check(action, direction):
-        # Sticky is a N hot encoded array for whether the player is performing an action
-        # Here, we check that the player is directed properly or we let it perform an action
+        '''
+        Check that the player controlled is directed properly before we let it perform an action
+        Args:
+            action: One of 19 actions for the player
+            direction: One of 9 actions
+        Returns: Cardinal direction
+        '''
+        # Sticky is a N hot encoded array of size 19 for whether the player is performing an action
+        # Like passing, shooting, runing right or left ...
         if direction in obs['sticky_actions'][0:9]:
+            # If the player is directed, perform an action
             return action
         else:
             return direction
 
     def expected_goal(player):
-        # Compute expected goals from paper :https://www.researchgate.net/publication/240641737_Estimating_the_probability_of_a_shot_resulting_in_a_goal_The_effects_of_distance_angle_and_space
+        '''
+        Compute expected goals from paper :https://www.researchgate.net/publication/240641737_Estimating_the_probability_of_a_shot_resulting_in_a_goal_The_effects_of_distance_angle_and_space
+        Args:
+            player: Controlled player coordinates
+        Returns: Float value representing probability of scoring
+        '''
         # Football rules: size of goal : 8 yard by 8 feet
         def to_yard(coord): return [coord[0] * 4 / 0.044, coord[1] * 4 / 0.044]
         # Get distance and angle to nearest post
         if (player[1] >= 0):
+            # Case where closer to the top post
             yards_to_goal = distance(to_yard(player), to_yard([1, 0.044]))
+            # Set the angle to 0 when in front of goal
             if (player[1] <= 0.044):
                 angle_to_goal = 0
             else:
                 angle_to_goal = abs(angle(player, [1, 0.044]))
         else:
+            # Case where closer to the bot post
             yards_to_goal = distance(to_yard(player), to_yard([1, -0.044]))
+            # Set the angle to 0 when in front of goal
             if (player[1] >= -0.044):
                 angle_to_goal = 0
             else:
@@ -89,42 +142,51 @@ def agent(obs):
         # Probability of scoring
         return np.exp(y) / (np.exp(y) + 1)
 
-    def opponent_on_path(controlled_player_position, teammate):
-        '''List of short pass'''
+    def opponent_on_path(player, teammate):
         # Find if there is an opponent player on the path of the path
-#         distances = obs['right_team'].copy()
+        if player[0] > teammate[0] and player[1] > teammate[1]:
+            # Case 3: BottomLeft pass
+            polygon = Polygon([(player[0] + body_radius * 2, player[1] - body_radius * 2),
+                               (player[0] - body_radius * 2,
+                                player[1] + body_radius * 2),
+                               (teammate[0] - body_radius * 2,
+                                teammate[1] + body_radius * 2),
+                               (teammate[0] + body_radius * 2, teammate[1] - body_radius * 2)])
+
+        elif player[0] < teammate[0] and player[1] > teammate[1]:
+            # Case 2: BottomRight pass
+            polygon = Polygon([(player[0] + body_radius * 2, player[1] + body_radius * 2),
+                               (player[0] - body_radius * 2,
+                                player[1] - body_radius * 2),
+                               (teammate[0] - body_radius * 2,
+                                teammate[1] - body_radius * 2),
+                               (teammate[0] + body_radius * 2, teammate[1] + body_radius * 2)])
+
+        elif player[0] > teammate[0] and player[1] < teammate[1]:
+            # Case 4: TopLeft pass
+            polygon = Polygon([(player[0] + body_radius * 2, player[1] + body_radius * 2),
+                               (player[0] - body_radius * 2,
+                                player[1] - body_radius * 2),
+                               (teammate[0] - body_radius * 2,
+                                teammate[1] - body_radius * 2),
+                               (teammate[0] + body_radius * 2, teammate[1] + body_radius * 2)])
+
+        elif player[0] < teammate[0] and player[1] < teammate[1]:
+            # Case 1: TopRight pass
+            polygon = Polygon([(player[0] - body_radius * 2, player[1] + body_radius * 2),
+                               (player[0] + body_radius * 2,
+                                player[1] - body_radius * 2),
+                               (teammate[0] + body_radius * 2,
+                                teammate[1] - body_radius * 2),
+                               (teammate[0] - body_radius * 2, teammate[1] + body_radius * 2)])
+
         for opponent in enumerate(obs[team]):
-            distances[i] = distance(
-                controlled_player_position, teammate_position)
-        return np.argmin(distances)
+            point = Point(opponent[0], opponent[1])
+            if polygon.contains(point):
+                return True
+        return False
 
-    constants = {
-        "delta_x": 9.2e-3,  # How much player / ball position changes when moving without sprinting
-        # How much player / ball position changes when sprinting
-        "sprinting_delta_x": 1.33e-2,
-        "max_distance_to_influence_ball": 0.01,
-        "max_distance_to_influence_ball_sprinting": 0.0185,
-        "goalie_out_of_box_dist": 0.3,
-        "on_breakaway_goalie_dist": 0.1
-    }
-
-    state = {
-        "prev_owner": None,
-        "count": 10,
-        "prev_player_pos": [0, 0],
-        "prev_ball_pos": [0, 0, 0]
-    }
-
-    dir_lookup = {
-        "vectors": np.array([[1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]),
-        "actions": [Action.Right, Action.TopRight, Action.Top, Action.TopLeft, Action.Left, Action.BottomLeft, Action.Bottom, Action.BottomRight]
-    }
-
-    def find_closest_opposition_to_ball(ball_pos, active_players):
-        '''Returns index of active player closest to the ball'''
-        pass
-
-    def pass_path():
+        def pass_path():
         pass
 
     def move_to_position(pos, player_pos):
@@ -154,7 +216,31 @@ def agent(obs):
         right_team_pos = obs["right_team"][1:]  # Not including the goalie
         return all([player_x > r_ply_pos[0] for r_ply_pos in right_team_pos])
 
-    # ___
+    # Constants
+
+    constants = {
+        "delta_x": 9.2e-3,  # How much player / ball position changes when moving without sprinting
+        # How much player / ball position changes when sprinting
+        "sprinting_delta_x": 1.33e-2,
+        "max_distance_to_influence_ball": 0.01,
+        "max_distance_to_influence_ball_sprinting": 0.0185,
+        "goalie_out_of_box_dist": 0.3,
+        "on_breakaway_goalie_dist": 0.1
+    }
+
+    state = {
+        "prev_owner": None,
+        "count": 10,
+        "prev_player_pos": [0, 0],
+        "prev_ball_pos": [0, 0, 0]
+    }
+
+    dir_lookup = {
+        "vectors": np.array([[1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]),
+        "actions": [Action.Right, Action.TopRight, Action.Top, Action.TopLeft, Action.Left, Action.BottomLeft, Action.Bottom, Action.BottomRight]
+    }
+
+    # Games State
     player_pos = obs['left_team'][obs['active']]
     prev_owner = state["prev_owner"]
     ball_owned_team = obs["ball_owned_team"]
@@ -162,7 +248,10 @@ def agent(obs):
     ball_pos = obs["ball"]
     ball_direction = obs["ball_direction"]
     ball_owned_player = obs["ball_owned_player"]
+    goal_post_top = [1, 0.044]
+    goal_post_bot = [1, -0.044]
 
+    # Game Agent
     if Action.Sprint not in obs['sticky_actions']:
         return Action.Sprint
 
@@ -175,21 +264,37 @@ def agent(obs):
             return direction_check(Action.ShortPass,
                                    coordinate_to_direction(player_pos,
                                                            closest_player(player_pos)))
+
         elif obs["game_mode"] == 2:
-            # GoalKick
-            return Action.LongPass
+            # GoalKick - Make a short pass to the closest teammate or make a long shot
+            teammate = closest_player(player_pos)
+            if opponent_on_path(player_pos, teammate):
+                return direction_check(Action.LongPass, Action.Right)
+            else:
+                return direction_check(Action.ShortPass,
+                                       coordinate_to_direction(player_pos, teammate))
+
         elif obs["game_mode"] == 3:
             # FreeKick
-            return Action.Shot
+            return direction_check(Action.Shot,
+                                   coordinate_to_direction(player_pos, [1, 0]))
+
         elif obs["game_mode"] == 4:
             # Corner
-            return Action.HighPass
+            return direction_check(Action.HighPass,
+                                   coordinate_to_direction(player_pos, [1, 0]))
+
         elif obs["game_mode"] == 5:
-            # ThrowIn
-            return Action.ShortPass
+            # ThrowIn - Make a short pass to the closest teammate
+            return direction_check(Action.ShortPass,
+                                   coordinate_to_direction(player_pos,
+                                                           closest_player(player_pos)))
+
         elif obs["game_mode"] == 6:
             # Penalty
-            return Action.Shot
+            return direction_check(Action.Shot,
+                                   coordinate_to_direction(player_pos, [1, 0]))
+
         else:
             # Normal
             # Calculate goalie position, if goalie presses could lead to loss of possesion
