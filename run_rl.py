88455d51b4e8a841b968b607b0ff34cc7bbc3332
Ali <alishobeiri@ymail.com>
2020-11-26 15:30:12 -0700

    Number of max trajectories reduced to 100

--- run_rl.py
+++ run_rl.py
@@ -128,7 +128,7 @@ def build_and_train(scenario="academy_empty_goal_close",
         max_decorrelation_steps=int(3000), # How many steps to take in env before training to randomize starting env state so experience isn't all the same
         eval_n_envs=100,
         eval_max_steps=int(100e5),
-        eval_max_trajectories=200,
+        eval_max_trajectories=100,
         **config["sampler"]  # More parallel environments for batched forward-pass.
     )
 
